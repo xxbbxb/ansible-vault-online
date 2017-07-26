c16f13b09aa27b5cecd96bbdaca2f1eca68f5788
Симонов Борис Сергеевич <simonov_bs@skbkontur.ru>
2017-07-26 21:02:11 +0500

    use relative url

--- web/vault.js
+++ web/vault.js
@@ -4,7 +4,7 @@ angular.module('vaultapp').controller('mainCtrl', function ($scope, $http) {
   $scope.crypt = function () {
     var data = {password: $scope.password, source: $scope.source};
     var config = { headers : { 'Content-Type': 'application/json;charset=utf-8;' } };
-    $http.post('/crypt', data, config)
+    $http.post('crypt', data, config)
       .then(function(resp) {
         $scope.result = resp.data.value;
       }, function(resp) {
