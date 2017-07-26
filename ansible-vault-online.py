e80d65ead03763983f92bd360ab68eb669e94c4d
simonovbs <simonov_bs@skbkontur.ru>
2017-07-26 19:42:06 +0500

    remove logging

--- ansible-vault-online.py
+++ ansible-vault-online.py
@@ -1,13 +1,10 @@
 
 from twisted.application import internet, service
 from twisted.web import server
-from twisted.python import log
 
 from scripts import resources
 
-
 app = service.MultiService()
-log.msg("test message")
 web_root = resources.Root('./web')
 web_root.putChild('crypt', resources.Crypt())
 web_factory = server.Site(web_root)
