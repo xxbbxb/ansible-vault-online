6aa8017cae5cd6dfefb0bf0a6fa8a9a39ade25c9
simonov_bs <simonov_bs@skbkontur.ru>
2018-02-22 17:31:43 +0500

    fix for ansible 2.4

--- scripts/resources.py
+++ scripts/resources.py
@@ -2,7 +2,9 @@ import json
 
 import ansible
 from twisted.web import static, resource
-from ansible.parsing.vault import VaultLib
+from ansible.parsing.vault \
+    import format_vaulttext_envelope, parse_vaulttext_envelope, VaultSecret, CIPHER_MAPPING, to_bytes
+
 
 class Root(static.File):
     isLeaf = False
@@ -14,11 +16,11 @@ class Root(static.File):
 class Crypt(resource.Resource):
     isLeaf = True
 
-    # noinspection PyBroadException
     def render(self, request):
 
         request.setHeader("Content-Type", "application/json; charset=utf-8")
-
+        version, cipher, vault_id = '1.1', 'AES256', ''
+        is_source_encrypted = True
         try:
             body = json.loads(request.content.read())
         except:
@@ -26,23 +28,34 @@ class Crypt(resource.Resource):
             return json.dumps({"value": "bad input object"})
 
         if body.get("password"):
-            e = VaultLib(body["password"])
+            secret = VaultSecret(to_bytes(body["password"], "utf-8", errors='strict'))
 
             source = body.get("source", "")
+            try:
+                (payload, version, cipher, vault) = parse_vaulttext_envelope(source)
+            except ansible.errors.AnsibleError:
+                # maybe not encrypted
+                is_source_encrypted = False
+                payload = to_bytes(source, "utf-8", errors='strict')
 
-            if source.startswith("$ANSIBLE_VAULT"):
-                try:
-                    response_text = e.decrypt(source)
-                except ansible.errors.AnsibleError as e:
-                    response_text = e.message
-            elif source == "":
-                response_text = "trying to encrypt empty string"
+            try:
+                this_cipher = CIPHER_MAPPING[cipher]()
+            except Exception as e:
                 request.setResponseCode(400)
-            else:
-                try:
-                    response_text = e.encrypt(source)
-                except ansible.errors.AnsibleError as e:
-                    response_text = e.message
+                response_text = e.message
+                return json.dumps({"value": "error in %s" % response_text})
+
+            try:
+                if is_source_encrypted:
+                    response_text = this_cipher.decrypt(payload, secret=secret)
+                else:
+                    response_text = format_vaulttext_envelope(this_cipher.encrypt(payload, secret=secret), cipher,
+                                                              version, vault_id)
+            except ansible.errors.AnsibleError as e:
+                request.setResponseCode(400)
+                response_text = e.message
+                return json.dumps({"value": response_text})
+
         else:
             request.setResponseCode(400)
             response_text = "password not specified"
